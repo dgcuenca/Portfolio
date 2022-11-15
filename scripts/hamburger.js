1576d4091a8d2bb57a780f6e83ba4ee098b49200
diego <dgcuenca@gmail.com>
2022-11-14 21:23:06 -0500

    Make interactive hamburger icon

---
+++ scripts/hamburger.js
@@ -0,0 +1,4 @@
+const hamburgerIcon=document.getElementById("hamburgerIcon");
+hamburgerIcon.addEventListener('click', () => {
+    document.getElementById("hamburgerDropdownContent").classList.toggle("show");
+  });

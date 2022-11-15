8a7646a44d4cc3931445d7d30cd47d4174a9c379
diego <dgcuenca@gmail.com>
2022-11-15 09:47:16 -0500

    Make dropdown disappear when click outside it

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,3 +2,8 @@ const hamburgerIcon=document.getElementById("hamburgerIcon");
 hamburgerIcon.addEventListener('click', () => {
     document.getElementById("hamburgerDropdownContent").classList.toggle("show");
   });
+
+// Close the dropdown menu if the user clicks outside of it
+window.addEventListener ('click', function(e){
+  if (!(e.target === hamburgerIcon)) hamburgerDropdownContent.classList.remove('show');
+});

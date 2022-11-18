e2bafff7cf579244f1c0a7c556628ec1f7d51baa
diego <dgcuenca@gmail.com>
2022-11-18 05:58:16 -0500

    Push see buttons to the end

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,9 +6,11 @@ const dataPopup = [
     language2: 'Bootstrap',
     language3: 'Ruby on rails',
     image: '../images/mobilePopup.png',
-    textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
+    textmulti: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever ",
     btnSeelive: 'See Live',
+    iconSeelive: '../icons/live.png',
     btnSeeSource: 'See Source',
+    iconSeeSource: '../icons/source.png',
   },
 
 ];
@@ -60,17 +62,25 @@ function displayLayoutPopup() {
 
   firstPopupContainer.appendChild(divmainImagen);
 
+  const divbottonPopup = document.createElement('div');
+  divbottonPopup.classList.add('containerBottonPopup');
+  firstPopupContainer.appendChild(divbottonPopup);
+
   const p = document.createElement('p');
   p.textContent = `${dataPopup[0].textmulti}`;
-  firstPopupContainer.appendChild(p);
+  divbottonPopup.appendChild(p);
+
+  const divSee = document.createElement('div');
+  divbottonPopup.appendChild(divSee);
+
 
   const btnSeelive = document.createElement('button');
   btnSeelive.textContent = `${dataPopup[0].btnSeelive}`;
-  firstPopupContainer.appendChild(btnSeelive);
+  divSee.appendChild(btnSeelive);
 
   const btnSeeSource = document.createElement('button');
   btnSeeSource.textContent = `${dataPopup[0].btnSeeSource}`;
-  firstPopupContainer.appendChild(btnSeeSource);
+  divSee.appendChild(btnSeeSource);
 }
 
 const firstButtonPopup = document.getElementById('firstButtonPopup');

e11681f244bcb288d008fb1790f62a262e22a1da
diego <dgcuenca@gmail.com>
2023-06-14 11:29:53 -0500

    Stay in same page position after close nav bar

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,10 +1,12 @@
 const hamburgerIcon = document.getElementById('hamburgerIcon');
 const hamburgerDropdownContent = document.getElementById('hamburgerDropdownContent');
+
 hamburgerIcon.addEventListener('click', () => {
   document.getElementById('hamburgerDropdownContent').classList.toggle('show');
 });
 
 // Close the dropdown menu if the user clicks outside of it
 window.addEventListener('click', (e) => {
+  e.preventDefault(); // because href="#" in HTML, prevent after close navigate to top
   if (!(e.target === hamburgerIcon)) hamburgerDropdownContent.classList.remove('show');
 });
